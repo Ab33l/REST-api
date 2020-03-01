ca0710a55386e5dd3f5caa814014a14b689444af
Ab33l <abell.wandili@strathmore.edu>
2020-03-01 14:55:11 +0300

    REST-API

---
+++ restful/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Employees(models.Model):
+    firstname = models.CharField(max_length=10)
+    lastname = models.CharField(max_length=10)
+    emp_id = models.IntegerField()
+
+    def __str__(self):
+        return self.firstname
+
+    class Meta:
+        verbose_name_plural = 'Employees'
